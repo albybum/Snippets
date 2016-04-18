c485e6008f277e58568cbd3dcdc75f188cd4b4aa
Holtsclaw <HOLTSCLAWA@etsu.edu>
2016-04-18 13:57:40 -0400

    adding .net mvc controllers stuff

---
+++ C#/DotNet MVC/Controllers/ControllerBase.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SecureAccess.Utilities;
+
+namespace CPS_Navigator.Controllers {
+    public class ControllerBase : Controller {
+
+        /// <summary>
+        /// Error Handler Override to intercept and log unhandled exceptions and to redirect to a friendly error page.
+        /// </summary>
+        /// <param name="filterContext"></param>
+        protected override void OnException(ExceptionContext filterContext) {
+            ModelStateDictionary model;
+            string input = string.Empty;
+            string method = string.Empty;
+            string controller = string.Empty;
+            string action = string.Empty;
+
+            // get method info from context
+            controller = filterContext.RouteData.Values["controller"] != null ? filterContext.RouteData.Values["controller"].ToString() : string.Empty;
+            action = filterContext.RouteData.Values["action"] != null ? filterContext.RouteData.Values["action"].ToString() : string.Empty;
+            method = string.Format("{0} -> {1}", controller, action);
+
+            // get input string from context
+            if (filterContext.Controller.ViewData != null && filterContext.Controller.ViewData.ModelState != null) {
+                model = filterContext.Controller.ViewData.ModelState;
+                foreach (KeyValuePair<string, ModelState> item in model) {
+                    string attmeptedValue = null;
+                    if (item.Value != null && item.Value.Value != null)
+                        attmeptedValue = item.Value.Value.AttemptedValue;
+
+                    input += string.Format("{0} = {1} ", item.Key, attmeptedValue);
+                }
+            }
+
+            // log error
+            Logging.LogException(
+                Logging.LogType.General,
+                string.Format("Unhandled Exception at [Method] {0} [Inputs] {1}", method, input).Truncate(1500),
+                filterContext.Exception,
+                "Exception",
+                Logging.Severity.Error);
+
+            // Output a nice error page
+            filterContext.ExceptionHandled = true;
+
+            if (filterContext.HttpContext.Request.IsAjaxRequest()) {
+                this.View("AjaxError").ExecuteResult(this.ControllerContext);
+            }
+            else
+                this.View("Error").ExecuteResult(this.ControllerContext);
+        }
+
+    }
+}
---
+++ C#/DotNet MVC/Controllers/FormsController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CPS_Navigator.Models;
+
+namespace CPS_Navigator.Controllers
+{
+
+    [Authorize(Roles = "Admin,Viewer")]
+    public class FormsController : ControllerBase
+    {
+        //
+        // GET: /Forms/
+        InputFormEntities _Forms = new Models.InputFormEntities();
+        ETSU_UsersEntities _Users = new Models.ETSU_UsersEntities();
+        WorkflowEntities _Workflows = new Models.WorkflowEntities();
+
+        public ActionResult Index()
+        {
+            List<FormTemplate> Templates = _Forms.FormTemplates.Where(e=>e.ProgramID == 5).ToList();
+            ViewBag.Templates = Templates;
+            List<SelectListItem> TemplateOptions = new List<SelectListItem>() { new SelectListItem() { Text = "- Select a Template -", Value="-1" } };
+            TemplateOptions.AddRange(Templates.Select(e => new SelectListItem() { Value = e.FormTemplateID.ToString(), Text = e.FormName }).ToList());
+            ViewBag.TemplateOptions = TemplateOptions;
+            return View();
+        }
+
+
+        public ActionResult InstancesByTemplate(int id) {
+            FormTemplate Template = _Forms.FormTemplates.Where(e=>e.FormTemplateID == id).SingleOrDefault();
+            List<FormInstance> Instances = _Forms.FormInstances.Where(e => e.FormTemplateID == id).OrderByDescending(e=>e.CreationTime).ToList();
+            ViewBag.Template = Template;
+            ViewBag.Instances = Instances;
+            return View();
+        }
+
+
+        public ActionResult FormInstanceData(int id) {
+            FormInstance Instance = _Forms.FormInstances.Where(e => e.FormInstanceID == id).SingleOrDefault();
+            List<FieldInstance> Fields = _Forms.FieldInstances.Where(e => e.FormInstanceID == id).OrderBy(e => e.FieldName).ToList();
+            ViewBag.Instance = Instance;
+            ViewBag.Fields = Fields;
+            return View();
+        }
+
+        public ActionResult Workflow(int id) {
+            tblWorkFlowInstance Workflow = _Workflows.tblWorkFlowInstances.Where(e => e.WorkflowInstanceID == id).SingleOrDefault();
+            List<tblStepInstanceSequence> StepInstanceSequences = _Workflows.tblStepInstanceSequences.Where(e => e.WorkflowInstanceID == id).OrderBy(e=>e.StepNumber).ToList();
+            List<int> InstanceIDs = StepInstanceSequences.Select(e=>e.StepInstanceID).ToList();
+            List<tblStepInstance> StepInstances = _Workflows.tblStepInstances.Where(e => InstanceIDs.Contains(e.StepInstanceID)).ToList();
+
+            ViewBag.Workflow = Workflow;
+            ViewBag.StepInstanceSequences = StepInstanceSequences;
+            ViewBag.StepInstances = StepInstances;
+            
+            return View();
+        }
+
+        public ActionResult SearchForms(int? TemplateID = null, string HasWorkflow = null, string FieldName = null, string FieldValue = null, int? FormInstanceID = null, int? WorkflowInstanceID = null, string Username = null) {
+            IQueryable<FormInstance> Instances = _Forms.FormInstances.AsQueryable();
+
+            if(TemplateID.HasValue && TemplateID.Value != -1)
+                Instances = Instances.Where(e => e.FormTemplateID == TemplateID).AsQueryable();
+
+            if (!string.IsNullOrEmpty(HasWorkflow)) {
+                if(HasWorkflow.Equals("Yes",StringComparison.CurrentCultureIgnoreCase)) {
+                    Instances = Instances.Where(e => e.WorkflowInstanceID.HasValue == true).AsQueryable();
+                }
+                if (HasWorkflow.Equals("No", StringComparison.CurrentCultureIgnoreCase)) {
+                    Instances = Instances.Where(e => e.WorkflowInstanceID.HasValue == false).AsQueryable();
+                }
+            }
+
+            if (FormInstanceID.HasValue)
+                Instances = Instances.Where(e => e.FormInstanceID == FormInstanceID.Value).AsQueryable();
+
+            if (WorkflowInstanceID.HasValue)
+                Instances = Instances.Where(e => e.WorkflowInstanceID == WorkflowInstanceID.Value).AsQueryable();
+
+            if (!string.IsNullOrEmpty(Username)) {
+                tblUserLanID usr = _Users.tblUserLanIDs.Where(e => e.LName.ToUpper() == Username.ToUpper()).SingleOrDefault();
+                if (usr != null) {
+                    Instances = Instances.Where(e => e.CreatedByUserID == usr.UserIdx).AsQueryable();
+                }
+            }
+
+            // least efficient, save for last
+            if (!string.IsNullOrEmpty(FieldName))
+                Instances = Instances.Where(e => e.FieldInstances.Where(f => f.FieldName.ToUpper().Contains(FieldName.ToUpper())).Count() > 0).AsQueryable();
+
+            if (!string.IsNullOrEmpty(FieldValue))
+                Instances = Instances.ToList().Where(e => e.FieldInstances.Where(f => f.FieldValue != null && f.FieldValue.ToUpper().Contains(FieldValue.ToUpper())).Count() > 0).AsQueryable();
+
+            ViewBag.Instances = Instances.OrderByDescending(e => e.CreationTime).ToList();
+            return View();
+        }
+
+
+    }
+}
